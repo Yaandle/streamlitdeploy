a4641f75f990f9ed5d66b3cdb7ee0f9f85de1962
Yaandle <zyandle03@gmail.com>
2024-04-02 19:50:13 +1100

    Update pages

--- home.py
+++ home.py
@@ -1,53 +1,34 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="MiFood",
+    page_title="S-U",
     page_icon="",
     layout="wide"
 )
 
-col1, col2 = st.columns([2, 3])
-with col1:
-    st.title("MiFood")
-    st.write("Welcome to our website! We are passionate about using robotics to improve efficiency and sustainability in the horticultural industry.")
-
-
-with st.expander("Vision and Mission", expanded=True):
-    st.write("""
-        Our vision is to create a future where robotics empowers the
-        horticultural industry, leading to increased productivity, reduced
-        waste, and a more sustainable food system. Our mission is to develop
-        and deploy innovative robotic solutions that address real-world
-        challenges faced by farmers and growers.
-    """)
-
-st.header("Our Programs")
-st.write("Discover our innovative tools for object detection and analysis:")
-
-with st.expander("ODIS Function"):
-    st.write("""
-        The Object Detection Image Sorting (ODIS) function is a tool designed for sorting images based on detected objects. Leveraging state-of-the-art object detection algorithms, ODIS can automatically organize your images, making it easier to manage and analyze large datasets.
-
-        **Key Features:**
-
-        * Automated image sorting based on object detection.
-        * Support for various object detection models.
-        * Efficient processing for large image datasets.
-    """)
-    st.markdown("[Go to ODIS Function](/ODIS)")
-
-
-with st.expander("MFIT Inference"):
-    st.write("""
-        MFIT is our specialized feature for object recognition and analysis. Whether you're working with single images, multiple images, or videos, MFIT offers robust inference capabilities, including detection and segmentation, to provide data on the inference.
-
-        **Key Features:**
-
-        * Support for inference on images, multi-images, and videos.
-        * Capabilities include both detection and segmentation.
-        * Ideal for testing and model analysis.
-    """)
-    st.markdown("[Go to MFIT](/MFIT)")
+st.title("Streamlit - Ultralytics app")
+st.write("Welcome to our website! We are passionate about using computer vision and robotics to improve efficiency.")
 
 st.divider()
-st.image("whitelogo.png", width=350)
+
+container1, container2 = st.columns(2)
+
+with container1:
+    st.subheader("Robots as a Service")
+    st.write("Use our robots to automate your workflow.")
+    st.page_link("pages/Service.py", label="Service", icon="🤖")
+
+    st.subheader("ODIS Function")
+    st.write("A tool for sorting images based on detected objects.")
+    st.page_link("pages/ODIS.py", label="ODIS", icon="🔁")
+
+with container2:
+    st.subheader("MFIT Inference")
+    st.write("MFIT is our specialized feature for object recognition and analysis.")
+    st.page_link("pages/MFIT.py", label="MFIT", icon="📸")
+
+    st.subheader("Technology")
+    st.write("Explore the technology used.")
+    st.page_link("pages/Technology.py", label="Technology", icon="🔧")
+
+st.divider()
--- pages/About.py
+++ pages/About.py
@@ -1,16 +1,12 @@
 import streamlit as st
-from pathlib import Path
 
-def run():
-    st.title("About MiFood")
+def about_mifood():
+    st.title("About our app.")
     with st.expander("Our Story"):
         st.write(
             """
             At MiFood, we are a team of passionate innovators dedicated to revolutionizing the horticultural industry through cutting-edge robotics and automation solutions. Our mission is to empower farmers and growers with the latest technological advancements, enabling them to overcome challenges, maximize efficiency, and pave the way for a more sustainable and prosperous future.
-            """
-        )
-        st.write(
-            """
+
             Born out of a deep respect for the vital role agriculture plays in our lives, we understand the complexities and labor-intensive nature of horticultural operations. By combining our expertise in robotics, computer vision, and artificial intelligence, we develop intelligent and autonomous systems that streamline processes, reduce manual labor, and optimize resource utilization.
             """
         )
@@ -21,60 +17,15 @@ def run():
             Our solutions are tailored to meet the unique needs of each client, offering customizable and scalable automation solutions that seamlessly integrate into existing operations. From precision planting and harvesting to crop monitoring and management, our range of services empowers growers to make informed decisions, increase yields, and minimize environmental impact.
             """
         )
-    
-    with st.expander("Our Team"):
-        st.write(
-            """
-            MiFood is driven by a diverse and talented team of engineers, scientists, and agricultural experts, united by a shared passion for innovation and a commitment to sustainable practices. Our multidisciplinary approach ensures that our solutions are not only technologically advanced but also deeply rooted in a comprehensive understanding of horticultural processes and challenges.
-            """
-        )
 
-    st.subheader("Industry Information")
-    st.write(
-        """
-        Stay informed about the latest trends, challenges, and opportunities shaping the horticultural industry. Our dedicated resource center provides in-depth analyses, case studies, and expert insights, empowering you with the knowledge to make informed decisions and stay ahead of the curve.
-        """
-    )
-    st.page_link("pages/Industry_Information.py", label=":blue[Learn More]")
-    st.divider()
-    st.subheader("Technology")
-    st.write(
-        """
-        At the heart of MiFood's solutions lies a fusion of cutting-edge technologies, including advanced robotics, computer vision, machine learning, and artificial intelligence. Our team of skilled engineers and scientists continuously push the boundaries of innovation, developing proprietary algorithms and hardware systems that redefine the capabilities of automated horticultural operations.
-        """
-    )
-    st.page_link("pages/Technology.py", label=":blue[Learn More]")
-    technology_image = Path("images/technology.jpg")
-    if technology_image.exists():
-        st.image(technology_image, caption="MiFood's advanced robotics technology")
-
-    st.divider()
-    st.subheader("Services")
-    services_cols = st.columns(1)
-    with services_cols[0]:
-        st.write(
-            """
-            Robot Leasing:
-            Our flexible robot leasing programs provide cost-effective access to our automated solutions, allowing you to scale your operations and stay ahead of the curve.
-            """
-            """
-            Customized Integration:
-            Our team of experts work closely with you to integrate our solutions seamlessly into your existing operations, ensuring a smooth transition and maximizing efficiency.
-            """
-        )
-        
-    st.page_link("pages/Service.py", label=":blue[Learn More]")
-    st.divider()
-
-
-    st.subheader("Join the revolution")
-    st.write(
-        """
-        We invite you to join us on this exciting journey as we redefine the future of horticulture. Together, we can unlock new levels of efficiency, productivity, and sustainability, ensuring a secure and abundant food supply for generations to come.
-        """
-    )
-
-    st.write("MiFood, Australia | Contact: zac@mifood.es")
+    with st.expander("Vision and Mission", expanded=True):
+        st.write("""
+            Our vision is to create a future where robotics empowers the
+            horticultural industry, leading to increased productivity, reduced
+            waste, and a more sustainable food system. Our mission is to develop
+            and deploy innovative robotic solutions that address real-world
+            challenges faced by farmers and growers.
+        """)
 
 if __name__ == "__main__":
-    run()
+    about_mifood()
